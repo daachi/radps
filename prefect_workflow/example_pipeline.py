5c7c9a68052e4069a852ba25a67d5aabc8af368c
kberry <kberry@nrao.edu>
2025-01-23 11:30:27 -0500

    Add initial placeholder file for a Prefect workflow based on the example pipeline in Figure 1 of 'An Example RADPS Workflow Decomposition'

---
+++ prefect_workflow/example_pipeline.py
@@ -0,0 +1,22 @@
+from prefect import flow, task
+
+# Implemetation of the example pipeline from Figure 1
+# of "An Example RADPS Workflow Decomposition"
+
+# Calibrator Data Import and Prep
+
+# Bandpass Solve
+
+# Time Gain Solve
+
+# Image Calibrators
+
+# Target Data Import and Prep
+
+# Calibrate Target and Find Continuum
+
+# Cube Imaging
+
+# Continuum Imaging with Self-Calibration
+
+# Per-SPW Continuum Imaging
