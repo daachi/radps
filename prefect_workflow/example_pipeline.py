afb79548d5b86df98fb9d864370f52a9f0df179d
amcnicho <amcnicho@nrao.edu>
2025-01-27 13:42:25 -0500

    Update example_pipeline.py
    Add some a dummy flow consisting of a single task that generates a data structure to pass around and fills it with a couple of random dask arrays

--- prefect_workflow/example_pipeline.py
+++ prefect_workflow/example_pipeline.py
@@ -1,3 +1,6 @@
+import time
+import dask.array as da
+import xarray as xr
 from prefect import flow, task
 
 # Implemetation of the example pipeline from Figure 1
@@ -12,6 +15,38 @@ from prefect import flow, task
 # Image Calibrators
 
 # Target Data Import and Prep
+@task
+def fake_archive_query() -> dict:
+    print("Pretending to fetch some data from an archive")
+    time.sleep(3)
+    print("Now that that latency simulation is complete, generating some mock data to return")
+    rng = da.random.default_rng()
+    target_vals = rng.standard_normal(size=(100,100,2,4))
+    calibrator_vals = rng.standard_normal(size=(100,100,10,4))
+
+    fake_result = {
+        "data" : {
+            "source_0" : calibrator_vals,
+            "source_1" : target_vals,
+        },
+        "data_source" : "archive",
+        "url" : "https://almascience.nrao.edu/aq/",
+        }
+    print("All done!")
+
+    return fake_result
+
+@flow
+def extract_transform_load() -> dict:
+    print("Calling archive query task")
+    fake_data = fake_archive_query()
+    print("Finished retrieving some object from that task function")
+    print("Performing a dummy sub-selection step")
+    time.sleep(2)
+    transformed_data = fake_data
+    transformed_data["data"].pop("source_1")
+
+    return transformed_data
 
 # Calibrate Target and Find Continuum
 
@@ -20,3 +55,6 @@ from prefect import flow, task
 # Continuum Imaging with Self-Calibration
 
 # Per-SPW Continuum Imaging
+
+if __name__ == "__main__":
+    extract_transform_load()
