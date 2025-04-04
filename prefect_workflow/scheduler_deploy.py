ac6adb26be292c3f10507ad609b8147070fe3f28
kberry <kberry@nrao.edu>
2025-04-04 10:57:38 -0400

    Add csv and table artifact output for flow scaling test.

--- prefect_workflow/scheduler_deploy.py
+++ prefect_workflow/scheduler_deploy.py
@@ -7,13 +7,13 @@ from core import sleep_placeholder
 
 
 @flow
-def flow_test(max_time=0.01):
-    elapsed = sleep_placeholder(max_time)
+def flow_test(min_time, max_time=0.01):
+    elapsed = sleep_placeholder(max_time, min_sleep=min_time)
     return elapsed
 
 
 if __name__ == "__main__":
 
-    flow_deploy = flow_test.to_deployment(name="flow overhead")
+    flow_deploy = flow_test.to_deployment(name="flow-overhead")
 
     serve(flow_deploy, limit=os.cpu_count())
--- prefect_workflow/scheduler_overhead.py
+++ prefect_workflow/scheduler_overhead.py
@@ -24,13 +24,46 @@ def task_test(min_time, max_time):
 
 
 @flow
-def flow_scaling_test(number_of_subflows):
+def flow_scaling_test(number_of_subflows, min_time=0.001, max_time=0.01):
     """
     Note: depends on scheduler_deploy.py having already been invoked, running as a background process.
     """
     print(f"Working on {number_of_subflows} flows in this flow invocation")
+    start = time.time()
+    results = []
     for tt in range(0, number_of_subflows):
-        run_deployment(name="flow-test/flow overhead", parameters={}, timeout=0)
+        flow_run = run_deployment(name="flow-test/flow-overhead", parameters={"min_time": min_time, "max_time": max_time}, timeout=0)
+        results.append(flow_run.total_run_time.total_seconds())
+    end = time.time()
+
+    T_sum_flow_times = sum(results)
+    T_workflow = end - start
+
+    pc = get_run_context()
+
+    timing_results = [
+        {
+            "date_and_time": datetime.now().isoformat(),
+            "developer": os.getlogin(),
+            "system_name": platform.node(),
+            "workflow": inspect.stack()[0][3],
+            "n_subflows": number_of_subflows,  # NOTE: n_subflows rather than n_tasks
+            "min_sleep": min_time,
+            "max_sleep": max_time,
+            "Wall clock time": T_workflow,
+            "Sum of sleep times": T_sum_flow_times,  # FIXME: this is clearly incorrect (always 0)
+            "n_threads": os.cpu_count(),
+            "n_processes": 1,  # think this should always be 1
+            "n_parallelism": os.cpu_count(),
+            "runner": str(type(pc.task_runner)),
+            "workflow_orchestration_framework": "prefect",
+            "backend_database": "postgres",
+            "workflow_type": "flow",  # populate via argument?
+            "total_memory": psutil.virtual_memory().total / (1024**3),
+        }
+    ]
+    create_table_artifact(table=timing_results)
+    return timing_results
 
 
 @flow(log_prints=True, task_runner=ThreadPoolTaskRunner(max_workers=os.cpu_count()))
@@ -43,9 +76,9 @@ def task_scaling_test(number_of_tasks=1000, min_time=0.001, max_time=0.01):
     for num_tasks in range(0, number_of_tasks):
         duration = task_test.submit(0.001, 0.01)
         results.append(duration.result())
+    end = time.time()
 
     T_sum_task_times = sum(results)
-    end = time.time()
 
     T_workflow = end - start
 
@@ -107,11 +140,21 @@ if __name__ == "__main__":
     print("Running flow_scaling_test")
     try:
         sizes = [1000, 2000, 4000, 8000, 16000]
+
+        overall_flow_scaling_results = []
         for size in sizes:
-            timings = flow_scaling_test(size)
+            timings = flow_scaling_test(size, 0.001, 0.01)
+            save_timing_results(pd.DataFrame.from_dict(timings))
+            overall_flow_scaling_results.append(timings[0])
+
     except exceptions.ObjectNotFound:
         print("Failed to find deployment expected by running scheduler_deploy.py")
         print("Skipping the rest of flow_scaling_test and proceeding")
+
+    # Artifact for overall flow_scaling_test results:
+    create_table_artifact(
+         table=overall_flow_scaling_results, key="flow-scaling-results"
+    )
 
     print("Running task_scaling_test")
     sizes = [1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000]
