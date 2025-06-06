2252a103a9b11cfa144444f10e0aa9e9bf5156e5
kberry <kberry@nrao.edu>
2025-06-06 09:43:31 -0400

    Add simulated error handling for bandpass_solve.

--- airflow_workflow/dags/bandpass_solve.py
+++ airflow_workflow/dags/bandpass_solve.py
@@ -1,7 +1,9 @@
 import datetime
 import pendulum
+import random
 import time
 
+from airflow.exceptions import AirflowException
 from airflow.sdk import dag, task
 
 @dag(
@@ -21,9 +23,13 @@ def bandpass_solve():
     @task
     def query_calmodel():
         time.sleep(1.0)
+        if random.choice([True, False]):
+            raise AirflowException("Simulated query_calmodel failure for testing")
+
         return True
 
     @task
+    @task(trigger_rule="none_failed_min_one_success")
     def predict_and_save_model_visibilities():
         time.sleep(1.0)
         return True
@@ -38,6 +44,20 @@ def bandpass_solve():
         time.sleep(1.0)
         return True
 
-    autoflag_bandpass_calc_src() >> query_calmodel() >> predict_and_save_model_visibilities() >> phase_only_solution_per_time() >> amp_and_phase_solution_across_scans()
-    
+    @task(trigger_rule="all_failed")
+    def use_backup_model(**context):
+        # runs if query calmodel() fails
+        print("Pretending to rolling back to a non-existent previous checkpoint")
+
+    task1 = autoflag_bandpass_calc_src()
+    task2 = query_calmodel()
+    task3 = predict_and_save_model_visibilities()
+    task4 = use_backup_model()
+    task5 = phase_only_solution_per_time()
+    task6 = amp_and_phase_solution_across_scans()
+
+    task1 >> task2 >> [task3, task4]
+    task4 >> task3 >> task5 >> task6
+
+
 dag = bandpass_solve()
