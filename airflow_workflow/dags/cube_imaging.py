fae404b5576e9854fa305b49d8587eab3bf02c91
Takahiro Tsutsumi <tak.tsutsumi@gmail.com>
2025-06-06 14:45:29 -0600

    Add TriggerDagRunOperator to trigger solver DAG

--- airflow_workflow/dags/cube_imaging.py
+++ airflow_workflow/dags/cube_imaging.py
@@ -1,11 +1,14 @@
 from airflow.sdk import dag, task, task_group
-from datetime import datetime
+from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.models import Variable
+from datetime import datetime, timedelta
 import random, time
 
 def  generate_qa(processname:str):
          qascore = random.uniform(0.0, 1.0)
          print(f"QA score for {processname} is {qascore}")
          return qascore
+
 
 @dag(
  dag_id="cube_imaging",
@@ -24,17 +27,30 @@ def cube_imaging():
     def extract_metadata(nchan=1)->dict:
         """ Extract metadata from the data  """
         fake_metadata = {
-                   'spwid': [0, 1, 2, 3],
-                    'channel': list(range(nchan))
+                    'field': 'target',
+                    'spw': [0, 1, 2, 3],
+                    'channel': list(range(nchan)),
+                    'scan': [0, 1, 2, 3, 4],
                     }
         return fake_metadata
 
+    # generate configuration (parameters) to send to the child DAG
+    @task
+    def make_conf(metadata:dict):
+        nfield= len(metadata['field'])
+        nspw = len(metadata['spw'])
+        nchan = len(metadata['channel'])
+        nscan = len(metadata['scan'])
+        niter = 5
+        return {'n_par': nfield*nspw,
+                'n_comb': nscan,
+                'niter':niter} # assuming a fixed number of scans for simplicity
     # dictionary returned by extract_metadata cannot be directly used
     # in the argument of .expand and refereces the key in the dictionary
     # as it is XCom object
     @task
     def get_spwlist(metadata:dict)->list:
-        return metadata['spwid']
+        return metadata['spw']
     
     @task
     def get_chanlist(metadata:dict)->list:
@@ -47,7 +63,6 @@ def cube_imaging():
         time.sleep(1.0)
         return True
 
-    
     @task
     def uvcontsub(spwid):
         print(f"Processing UV continuum subtraction for spw: {spwid}")
@@ -55,14 +70,13 @@ def cube_imaging():
         return True
 
 
-    
     @task.branch
     def check_qa_branch(process:str, next_branch_taskname:str):
         """ branch task based on qa score"""
         qascore = generate_qa(process)
         if qascore > 0.3:
             print(f"QA score = {qascore} for {process} PASS")
-            return f"{next_branch_taskname}_group"
+            return f"{next_branch_taskname}"
         else:
             return "finalize_task"
              
@@ -79,17 +93,25 @@ def cube_imaging():
             print("QA FAIL")
             return False
         
-    @task_group(group_id='image_target_cube_group')
-    def image_target_cube_group():
-        meta_data = extract_metadata(10)
-        chanlist = get_chanlist(meta_data)
+    meta_data = extract_metadata(10)
+    #get_chanlist(meta_data)
+    dag_conf = make_conf(meta_data)
 
-        @task(task_id='image_target_cube')
-        def image_target_cube(chan):
-            print(f"Processing cube imaging channel: {chan} for cube imaging")
-            return True
-    
-        mapped_image_cube = image_target_cube.expand(chan=chanlist)
+
+    # solver dag trigger version
+    image_target_cube = TriggerDagRunOperator(
+        task_id='image_target_cube',
+        trigger_dag_id='solver_new',
+        conf=dag_conf,
+        wait_for_completion=True,
+        poke_interval=30,
+        execution_timeout=timedelta(600),
+        allowed_states=['success'],
+        failed_states=['failed'],
+    )
+
+        
+        #mapped_image_cube = image_target_cube.expand(chan=chanlist)
     
     
 
@@ -101,21 +123,18 @@ def cube_imaging():
         return True
     
     #chunklist = extract_metadata()['chunkid']\
-
-    meta_data = extract_metadata(10)
     spwlist= get_spwlist(meta_data)
     
     # use override to change the generic task_id to specific task_id 
     post_uvcontsub_branch = check_qa_branch.override(task_id='post_uvcontsub_branch')(process="uvcontsub", next_branch_taskname="image_target_cube")
     finalize_task = finalize_op()
-    #image_target_cube_mapped = image_target_cube.expand(chan=chanlist)
-    image_target_cube_branch = image_target_cube_group()
+    image_target_cube_branch = image_target_cube
+
     
     data_prep() >> meta_data >> spwlist >> uvcontsub.expand(spwid=spwlist)>> post_uvcontsub_branch
     post_uvcontsub_branch >> image_target_cube_branch >> check_qa('target_image') >> finalize_task
     post_uvcontsub_branch >> finalize_task
 
 
-    #data_prep ()  >> meta_data >> spwlist >> chanlist >>  uvcontsub.expand(spwid=spwlist) >> image_target_cube.expand(chan=chanlist)
-
 cube_imaging()
+
